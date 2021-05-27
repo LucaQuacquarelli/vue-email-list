4c346fa9ce642be32441fd86d733bc398613ef74
LucaQuacquarelli <lucaq82@libero.it>
2021-05-27 15:02:01 +0200

    Stampa mail generata dall' API + Bonus

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            mailList: []
+        },
+        created: function() {
+            for (var i = 0; i < 10; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(
+                    (result) => {
+                        this.mailList.push(result.data.response);
+                    }
+                )
+            }
+        }
+    }
+)
